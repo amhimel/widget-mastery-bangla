94baf9e32bda794e00e8cd4ddd265dd1749c52f0
amhimel <md.abdullaalmasudhimel@gmail.com>
2025-11-16 21:04:50 +0600

    day01

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MaterialApp(
+    title: 'Widget Mastery In Bangla',
+    theme: ThemeData(
+      primarySwatch: Colors.amber
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Appbar"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(child: Text("Body text")),
+    ),
+
+  ));
 }
 
