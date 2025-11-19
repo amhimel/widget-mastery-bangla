ec1ee9df8fe55c25f8dce5291fdb28de2bc9a7a0
amhimel <md.abdullaalmasudhimel@gmail.com>
2025-11-19 19:38:16 +0600

    day02

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Widget Mastery In Bangla',
-    theme: ThemeData(
-      primarySwatch: Colors.amber
-    ),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Appbar"),
-        backgroundColor: Colors.red,
+  runApp(
+    MaterialApp(
+      title: 'Material App',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AppBar & SafeArea'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+          ],
+        ),
+        body: SafeArea(
+          left: true,
+            right: false,
+            top: false,
+            bottom: true,
+            child: Text("dataglfsdflkgsdlfkgnhnnnjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"))
       ),
-      body: Center(child: Text("Body text")),
     ),
-
-  ));
+  );
 }
-
