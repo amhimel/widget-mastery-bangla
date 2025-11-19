c8602b9f3bbe9e50847a92de981a0ba01b7a862a
amhimel <md.abdullaalmasudhimel@gmail.com>
2025-11-19 19:58:02 +0600

    day02

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,22 @@ void main() {
       title: 'Material App',
       theme: ThemeData(primarySwatch: Colors.amber),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('AppBar & SafeArea'),
-          centerTitle: true,
-          backgroundColor: Colors.redAccent,
-          actions: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-          ],
-        ),
-        body: SafeArea(
-          left: true,
-            right: false,
-            top: false,
-            bottom: true,
-            child: Text("dataglfsdflkgsdlfkgnhnnnjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"))
+          appBar: AppBar(
+            title: Text('AppBar & SafeArea'),
+            centerTitle: true,
+            backgroundColor: Colors.redAccent,
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+            ],
+          ),
+          body: SafeArea(
+              top: true,
+              bottom: true,
+              left: false,
+              right: false,
+              child: Text(
+              'Multiline text is a block of text that spans multiple lines within a defined boundary, like a text box or a specified area. Unlike single-line text, it automatically wraps words to the next line and allows for features such as formatting, justification, and bulleted lists, similar to a word processor. This is used for extensive notes or messages in applications and design software'))
       ),
     ),
   );
